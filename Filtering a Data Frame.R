ef05795ff891a29ffa50c9ad5a9997321ad2bbca
AlistairGJ <AlistairGJ@gmail.com>
2017-09-29 14:46:19 +1000

    Update

--- Filtering a Data Frame.R
+++ Filtering a Data Frame.R
@@ -1,3 +1,4 @@
+
 #------------Packages + Data
 library(ggplot2)
 library(dplyr)
@@ -7,7 +8,9 @@ library(gridExtra)
 library("htmltools")
 df <- gsheet2tbl('https://drive.google.com/open?id=1_6RZ0HXg7VbgO78oyFfUyZaFOUHYWijy8XH1_jw5tA4')
 #------------Filtering Dataframes
-##
+##More about rows - e.g., only want to look at specific rows
+###Specific values - Returns TRUE & FALSE
+df$Coles > 10
 
 
 
