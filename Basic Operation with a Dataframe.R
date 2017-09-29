94ce023819a71ae9326de3b5618f2b7a55fb5f1d
AlistairGJ <AlistairGJ@gmail.com>
2017-09-29 14:35:01 +1000

    Update

--- Basic Operation with a Dataframe.R
+++ Basic Operation with a Dataframe.R
@@ -11,5 +11,25 @@ df <- gsheet2tbl('https://drive.google.com/open?id=1_6RZ0HXg7VbgO78oyFfUyZaFOUHY
 df[1:10,]
 ##Third to ninth row
 df[3:9,]
-##Not maintaining the row number from the original df...?
+##Not maintaining the row number from the original df...? Why are the numbers of the rows not maintained?
+##Possibly due to import from Google docs?!?
+df[c(4,100),]
+#Remeber how the [] work
+##First row only
+df[1,]
+##Checking if dataframe
+is.data.frame(df[1,])
+##Extracting just the first column - extracted below as data frame
+df[,1]
+is.data.frame(df[,1])
+df[,5]
+##Add Column - below
+df$NewColumn <- 0
+#Add Column of alternating values
+df$Alternating <- 1:5
+#Removing columns
+df$NewColumn <- NULL
+##Gone!
+df$Alternating <- NULL
+
 
